dc4a49cc41c620eb6737df5734a2f9c76ba5388f
phaptrinh <phap.n.trinh00@gmail.com>
2022-01-23 13:56:08 +0700

    create UserRole entity

---
+++ src/main/java/com/example/myedu/entity/UserRole.java
@@ -0,0 +1,25 @@
+package com.example.myedu.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class UserRole {
+    @Id
+    @GeneratedValue
+    private int userRoleId;
+
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "roleId")
+    private Role role;
+}
